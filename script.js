66efd466ef1214117d65c66e59696fab8c91e09d
yashdatir <yashdatir@gmail.com>
2021-11-09 13:49:57 +0530

    added loader, improved counter, UI

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-const DAYS = 1000 * 24 * 60 * 60;
-const HPD = DAYS;
-const MPD = 1000;
+const seconds = 1000;
+const minutes = seconds * 60;
+const hours = minutes * 60;
+const days = hours * 24;
 
-const birthDay = new Date('2021-11-10');
+const birthDay = new Date('Nov 10, 2021 00:00:00').getTime();
 var timer = document.querySelector('.timer');
 var counter = document.querySelector('.counter');
 var mainPage = document.querySelector('.main');
 mainPage.style.display = 'none';
-const dayDiff = () => {
-  var diff = birthDay - new Date();
-  const days = Math.floor(diff / DAYS);
-  const hours = Math.floor(diff / HPD);
-  console.log(days, hours);
-};
 var tmr = setInterval(() => {
-  const time = new Date().getSeconds();
-  dayDiff();
-  timer.innerHTML = time;
-  if (time === 30) {
+  let now = new Date().getTime();
+  const difference = birthDay - now;
+  timer.innerHTML = ` ${Math.floor((difference % days) / hours)} : ${Math.floor((difference % hours) / minutes) } : ${Math.floor((difference % minutes) / seconds) } `;
+  if (difference <= 0) {
     counter.style.display = 'none';
     mainPage.style.display = 'flex';
     return clearInterval(tmr);

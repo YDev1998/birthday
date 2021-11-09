a6bc89ce241cc901a09e358d021be238b15a8ee4
Yash <yashdatir@Yashs-MacBook-Pro.local>
2021-11-09 12:29:43 +0530

    intermediate commit

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-var counter = document.querySelector('.counter');
-var main = document.querySelector('.main');
+const DAYS = 1000 * 24 * 60 * 60;
+const HPD = DAYS;
+const MPD = 1000;
+
+const birthDay = new Date('2021-11-10');
 var timer = document.querySelector('.timer');
+var counter = document.querySelector('.counter');
+var mainPage = document.querySelector('.main');
+mainPage.style.display = 'none';
+const dayDiff = () => {
+  var diff = birthDay - new Date();
+  const days = Math.floor(diff / DAYS);
+  const hours = Math.floor(diff / HPD);
+  console.log(days, hours);
+};
+var tmr = setInterval(() => {
+  const time = new Date().getSeconds();
+  dayDiff();
+  timer.innerHTML = time;
+  if (time === 30) {
+    counter.style.display = 'none';
+    mainPage.style.display = 'flex';
+    return clearInterval(tmr);
+  }
+}, 1000);
